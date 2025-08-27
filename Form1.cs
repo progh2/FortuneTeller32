a5729973adfe9bee40e01725fb48f676f9f9c723
Gihun Ham <progh2@gmail.com>
2025-08-27 13:43:03 +0900

    #2 중복으로 내역 불러오기 창이 뜯는 것을 수정
    내역 불러오기 로직 개선
    
    `내열불러오기ToolStripMenuItem_Click` 메서드에서
    `FormHistory` 폼이 이미 열려 있는지 확인하는 로직을 추가했습니다.
    이제 열려 있는 폼이 있을 경우 해당 폼을 활성화하고,
    없을 경우에만 새로운 인스턴스를 생성하여 보여줍니다.
    이 변경은 사용자 경험을 개선하고 중복된 폼 생성을 방지합니다.

--- Form1.cs
+++ Form1.cs
@@ -19,8 +19,16 @@ namespace FortuneTeller32
 
         private void 내열불러오기ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            FormHistory form = new FormHistory();
-            form.Show();
+            FormHistory form = Application.OpenForms["FormHistory"] as FormHistory;
+            if (form != null)
+            {
+                form.Activate();
+            }
+            else
+            {
+                form = new FormHistory();
+                form.Show();
+            }
         }
 
         private void 끝내ToolStripMenuItem_Click(object sender, EventArgs e)
